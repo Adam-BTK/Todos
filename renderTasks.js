e65513494869520391801ac87f94f5a084c9fbc7
Адам Батукаев <mcxchr@yandex.ru>
2023-08-13 15:35:17 +0300

    ToDosPrj_Node_js

--- renderTasks.js
+++ renderTasks.js
@@ -1,22 +1,36 @@
 // Модуль renderTasks.js
 import { deleteTodo, postTodo } from "./api.js";
+import { format } from "date-fns";
 
-const listElement = document.getElementById("list");
+// const listElement = document.getElementById("list");
 
 export const renderTasks = ({ tasks, fetchAndRenderTasks }) => {
   const appElement = document.getElementById('app');
 
+  // const formatDate = (date) => {
+  //   return `${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}/${date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()}/${date.getFullYear()} ${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`;
+  // }
+  
+  // const country = "ru";
   const tasksHtml = tasks
-    .map((task) => {
-      return `
-      <li class="task">
-        <p class="task-text">
-          ${task.text}
-          <button data-id="${task.id}" class="button delete-button">Удалить</button>
-        </p>
-      </li>`;
-    })
-    .join("");
+  .map((task) => {
+		// Вызываем функцию format из date-fns, первый параметр — это дата, которую
+		// хотим отформатировать, второй параметр — это строка: к какому формату
+		// желаем привести дату. Обратите внимание MM — это номер месяца,
+		// mm — это минуты
+    const createDate = format(new Date(task.created_at), 'dd/MM/yyyy hh:mm');
+    return `
+        <li class="task">
+          <p class="task-text">
+            ${task.text} (Создал: ${task.user?.name ?? "Неизвестно"})
+            <button data-id="${
+              task.id
+            }" class="button delete-button">Удалить</button>
+          </p>
+          <p><i>Задача создана: ${createDate}</i></p>
+        </li>`;
+  })
+  .join("");
 
   const appHtml = `<h1>Список задач</h1>
 <ul class="tasks" id="list">${tasksHtml}</ul>
